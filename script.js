6123b74cb3e3a662146e311f3865e5badfbcb5e3
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-01-31 20:56:42 +0530

    game logic works

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
 "use strict";
 class Game {
     constructor() {
-        this.pressGreen = document.querySelector('.press--green');
-        this.pressRed = document.querySelector('.press--red');
-        this.pressBlue = document.querySelector('.press--blue');
-        this.pressYellow = document.querySelector('.press--yellow');
-        this.btnToggle = document.querySelector('.btn-toggle');
-        this.greenAudio = new Audio('assets/sounds/green.mp3');
-        this.redAudio = new Audio('assets/sounds/red.mp3');
-        this.blueAudio = new Audio('assets/sounds/blue.mp3');
-        this.yellowAudio = new Audio('assets/sounds/yellow.mp3');
+        this.presses = document.querySelectorAll(".press");
+        this.pressGreen = document.querySelector(".press--green");
+        this.pressRed = document.querySelector(".press--red");
+        this.pressBlue = document.querySelector(".press--blue");
+        this.pressYellow = document.querySelector(".press--yellow");
+        this.btnToggle = document.querySelector(".btn-toggle");
+        this.greenAudio = new Audio("assets/sounds/green.mp3");
+        this.redAudio = new Audio("assets/sounds/red.mp3");
+        this.blueAudio = new Audio("assets/sounds/blue.mp3");
+        this.yellowAudio = new Audio("assets/sounds/yellow.mp3");
+        this.gameOverAudio = new Audio("assets/sounds/wrong.mp3");
+        this.audioList = [
+            this.greenAudio,
+            this.redAudio,
+            this.blueAudio,
+            this.yellowAudio,
+        ];
         this.isRunning = false;
-        this.currentIndex = 0;
+        this.currentIndex = -1;
         this.currentStack = [];
-        this.pressGreen.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.greenAudio.play();
-            }
-        });
-        this.pressRed.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.redAudio.play();
-            }
-        });
-        this.pressBlue.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.blueAudio.play();
-            }
-        });
-        this.pressYellow.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.yellowAudio.play();
-            }
-        });
-        this.btnToggle.addEventListener('click', (e) => {
+        for (let i = 0; i < 4; i++) {
+            this.presses[i].addEventListener("click", () => {
+                if (this.isRunning) {
+                    this.audioList[i].play();
+                    if (this.currentStack[this.currentIndex] == i) {
+                        if (this.currentStack.length == this.currentIndex + 1) {
+                            setTimeout(() => {
+                                this.getNew();
+                            }, 500);
+                        }
+                    }
+                    else {
+                        this.gameOver();
+                    }
+                    this.currentIndex++;
+                }
+            });
+        }
+        this.btnToggle.addEventListener("click", (e) => {
             this.isRunning = !this.isRunning;
+            if (this.isRunning) {
+                this.getNew();
+            }
         });
     }
     getNew() {
         var newEntry = Math.round(Math.random() * 4);
+        newEntry = newEntry == 4 ? 3 : newEntry;
         this.currentStack.push(newEntry);
-        this.currentIndex++;
+        this.audioList[newEntry].play();
+        this.presses[newEntry].classList.add("on");
+        setTimeout((index) => {
+            this.presses[index].classList.remove("on");
+        }, 500, newEntry);
+        this.currentIndex = 0;
         return newEntry;
     }
     getCurrent() {
         return this.currentIndex;
     }
     gameOver() {
-        this.currentIndex = 0;
+        this.gameOverAudio.play();
+        this.currentIndex = -1;
         this.currentStack.length = 0;
+        this.isRunning = false;
     }
 }
 var game = new Game();
--- script.ts
+++ script.ts
@@ -1,55 +1,77 @@
 class Game {
     constructor() {
-        this.pressGreen.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.greenAudio.play();
-            }
-        });
+        for (let i: number = 0; i < 4; i++) {
+            this.presses[i].addEventListener("click", () => {
+                if (this.isRunning) {
+                    this.audioList[i].play();
 
-        this.pressRed.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.redAudio.play();
-            }
-        });
+                    if (this.currentStack[this.currentIndex] == i) {
+                        if (this.currentStack.length == this.currentIndex + 1) {
+                            setTimeout(() => {
+                                this.getNew();
+                            }, 500);
+                        }
+                    } else {
+                        this.gameOver();
+                    }
 
-        this.pressBlue.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.blueAudio.play();
-            }
-        });
+                    this.currentIndex++;
+                }
+            });
+        }
 
-        this.pressYellow.addEventListener('click', (e) => {
-            if (this.isRunning) {
-                this.yellowAudio.play();
-            }
-        });
-
-        this.btnToggle.addEventListener('click', (e) => {
+        this.btnToggle.addEventListener("click", (e) => {
             this.isRunning = !this.isRunning;
+
+            if (this.isRunning) {
+                this.getNew();
+            }
         });
     }
 
-    pressGreen: HTMLElement = document.querySelector('.press--green')!;
-    pressRed: HTMLElement = document.querySelector('.press--red')!;
-    pressBlue: HTMLElement = document.querySelector('.press--blue')!;
-    pressYellow: HTMLElement = document.querySelector('.press--yellow')!;
+    presses: NodeListOf<HTMLElement> = document.querySelectorAll(".press")!;
 
-    btnToggle: HTMLElement = document.querySelector('.btn-toggle')!;
+    pressGreen: HTMLElement = document.querySelector(".press--green")!;
+    pressRed: HTMLElement = document.querySelector(".press--red")!;
+    pressBlue: HTMLElement = document.querySelector(".press--blue")!;
+    pressYellow: HTMLElement = document.querySelector(".press--yellow")!;
 
-    greenAudio: HTMLAudioElement = new Audio('assets/sounds/green.mp3');
-    redAudio: HTMLAudioElement = new Audio('assets/sounds/red.mp3');
-    blueAudio: HTMLAudioElement = new Audio('assets/sounds/blue.mp3');
-    yellowAudio: HTMLAudioElement = new Audio('assets/sounds/yellow.mp3');
+    btnToggle: HTMLElement = document.querySelector(".btn-toggle")!;
+
+    greenAudio: HTMLAudioElement = new Audio("assets/sounds/green.mp3");
+    redAudio: HTMLAudioElement = new Audio("assets/sounds/red.mp3");
+    blueAudio: HTMLAudioElement = new Audio("assets/sounds/blue.mp3");
+    yellowAudio: HTMLAudioElement = new Audio("assets/sounds/yellow.mp3");
+    gameOverAudio: HTMLAudioElement = new Audio("assets/sounds/wrong.mp3");
+
+    audioList: HTMLAudioElement[] = [
+        this.greenAudio,
+        this.redAudio,
+        this.blueAudio,
+        this.yellowAudio,
+    ];
 
     isRunning: boolean = false;
-    currentIndex: number = 0;
+    currentIndex: number = -1;
     currentStack: number[] = [];
 
     getNew(): number {
         var newEntry: number = Math.round(Math.random() * 4);
+        newEntry = newEntry == 4 ? 3 : newEntry;
         this.currentStack.push(newEntry);
-        this.currentIndex++;
 
+        this.audioList[newEntry].play();
+        this.presses[newEntry].classList.add("on");
+
+        setTimeout(
+            (index: number) => {
+                this.presses[index].classList.remove("on");
+            },
+            500,
+            newEntry
+        );
+
+        this.currentIndex = 0;
         return newEntry;
     }
 
@@ -58,8 +80,10 @@ class Game {
     }
 
     gameOver(): void {
-        this.currentIndex = 0;
+        this.gameOverAudio.play();
+        this.currentIndex = -1;
         this.currentStack.length = 0;
+        this.isRunning = false;
     }
 }
 
